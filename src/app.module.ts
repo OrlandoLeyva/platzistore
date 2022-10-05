2e02305fd901fcf315e14ad6c7dd0457ecc0c8a3
orlando leyva <=>
2022-10-05 12:49:07 -0700

    controller and providers for products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './modules/products/controllers/products/products.controller';
 import { UsersController } from './modules/users/controllers/users/users.controller';
 import { CategoriesController } from './modules/products/controllers/categories/categories.controller';
 import { OrdersController } from './modules/users/controllers/orders/orders.controller';
@@ -10,12 +9,12 @@ import { BrandsController } from './modules/products/controllers/brands/brands.c
 import { ProductsService } from './modules/products/services/products/products.service';
 import { UsersService } from './modules/users/services/users/users.service';
 import { ProductsModule } from './modules/products/products.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, UsersModule],
   providers: [AppService, ProductsService, UsersService],
   controllers: [
-    ProductsController,
     UsersController,
     CategoriesController,
     OrdersController,
--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProductsController } from './controllers/products/products.controller';
+import { CategoriesController } from './controllers/categories/categories.controller';
+import { ProductsService } from './services/products/products.service';
 
-@Module({})
+@Module({
+  controllers: [ProductsController],
+  providers: [ProductsService],
+})
 export class ProductsModule {}
